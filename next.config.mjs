ba2281370a3005eeeb1ea6b350dcdb482c25c3df
essyriseelectronics <essyriseelectronics@gmail.com>
2026-09-19 16:31:17 +0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next/next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.r2.cloudflarestorage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-*.r2.dev',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
