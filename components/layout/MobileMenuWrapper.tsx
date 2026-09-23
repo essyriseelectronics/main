e13c1cc73144c5868d6ac7c71c3341b6eacab04d
essyriseelectronics <essyriseelectronics@gmail.com>
2026-09-23 22:30:46 +0300

    Update MobileMenuWrapper.tsx

--- components/layout/MobileMenuWrapper.tsx
+++ components/layout/MobileMenuWrapper.tsx
@@ -1,11 +1,28 @@
 'use client'
 
+import { useState } from 'react';
+import { Menu } from 'lucide-react';
 import MobileMenu from './MobileMenu';
 import { Category } from '@/types';
 
 type MobileMenuWrapperProps = {
   categories: Category[];
 };
+
 export default function MobileMenuWrapper({ categories }: MobileMenuWrapperProps) {
-  return <MobileMenu categories={categories} />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button 
+        onClick={() => setIsOpen(true)}
+        className="p-1 text-gray-800 focus:outline-none cursor-pointer"
+        aria-label="Open Menu"
+      >
+        <Menu className="h-7 w-7" strokeWidth={2.5} />
+      </button>
+
+      <MobileMenu categories={categories} isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
+  );
 }
