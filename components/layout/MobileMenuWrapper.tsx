29379afa9b7d77a3124dd5e358117f5346979907
essyriseelectronics <essyriseelectronics@gmail.com>
2026-09-23 22:13:08 +0300

    Update MobileMenuWrapper.tsx

--- components/layout/MobileMenuWrapper.tsx
+++ components/layout/MobileMenuWrapper.tsx
@@ -6,7 +6,6 @@ import { Category } from '@/types';
 type MobileMenuWrapperProps = {
   categories: Category[];
 };
-
 export default function MobileMenuWrapper({ categories }: MobileMenuWrapperProps) {
   return <MobileMenu categories={categories} />;
 }
